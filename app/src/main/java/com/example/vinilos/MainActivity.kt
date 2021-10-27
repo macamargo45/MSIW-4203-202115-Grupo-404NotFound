d40d6886258defd077125d6629198f61d7892096
Andres Benavides <andres.benavides@eventsite.co>
2021-10-26 19:32:57 -0500

    first commit for vinilos

---
+++ app/src/main/java/com/example/vinilos/MainActivity.kt
@@ -0,0 +1,20 @@
+package com.example.vinilos
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import androidx.appcompat.app.ActionBar
+import com.google.android.material.bottomnavigation.BottomNavigationView
+import com.google.android.material.navigation.NavigationBarView
+
+class MainActivity : AppCompatActivity() {
+
+    lateinit var toolbar: ActionBar
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+
+        toolbar = supportActionBar!!
+        val bottomNavigation: BottomNavigationView = findViewById(R.id.navigationView)
+    }
+}
