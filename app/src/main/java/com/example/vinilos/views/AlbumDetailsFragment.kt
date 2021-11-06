f66a10957efc0a3e9668444ffa4f760c9b1c1c19
jmonterovalverdeMISO <j.monterov@uniandes.edu.co>
2021-11-05 22:31:21 -0600

    UI Detalle de album completa

--- app/src/main/java/com/example/vinilos/views/AlbumDetailsFragment.kt
+++ app/src/main/java/com/example/vinilos/views/AlbumDetailsFragment.kt
@@ -1,11 +1,14 @@
 package com.example.vinilos.views
 
+import android.os.Build
 import android.os.Bundle
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import android.widget.ImageView
 import android.widget.TextView
 import android.widget.Toast
+import androidx.annotation.RequiresApi
 import androidx.fragment.app.Fragment
 import androidx.lifecycle.Observer
 import androidx.lifecycle.ViewModelProvider
@@ -16,11 +19,13 @@ import com.example.vinilos.R
 import com.example.vinilos.databinding.AlbumDetailsFragmentBinding
 import com.example.vinilos.models.Album
 import com.example.vinilos.viewmodels.AlbumDetailsViewModel
+import com.squareup.picasso.Picasso
+import java.time.LocalDate
+import java.time.format.DateTimeFormatter
 
 class AlbumDetailsFragment : Fragment() {
     private var _binding: AlbumDetailsFragmentBinding? = null
     private val binding get() = _binding!!
-    private lateinit var recyclerView: RecyclerView
     private lateinit var viewModel: AlbumDetailsViewModel
     private val args: AlbumDetailsFragmentArgs by navArgs()
 
@@ -28,7 +33,7 @@ class AlbumDetailsFragment : Fragment() {
     override fun onCreateView(
         inflater: LayoutInflater, container: ViewGroup?,
         savedInstanceState: Bundle?
-    ): View? {
+    ): View {
 
 
         _binding = AlbumDetailsFragmentBinding.inflate(inflater, container, false)
@@ -42,26 +47,47 @@ class AlbumDetailsFragment : Fragment() {
             "You can only access the viewModel after onActivityCreated()"
         }
         viewModel = ViewModelProvider(this, AlbumDetailsViewModel.Factory(activity.application)).get(AlbumDetailsViewModel::class.java)
-        viewModel.album.observe(viewLifecycleOwner, Observer<Album> {
+        viewModel.album.observe(viewLifecycleOwner, {
             it.apply {
                 //Log.d("act", "ACA CREO QUE DEBERIA CARGAR LOS ALBUMES")
                 //viewModelAdapter!!.albums = args
             }
         })
-        viewModel.eventNetworkError.observe(viewLifecycleOwner, Observer<Boolean> { isNetworkError ->
+        viewModel.eventNetworkError.observe(viewLifecycleOwner, { isNetworkError ->
             if (isNetworkError) onNetworkError()
         })
     }
 
+    @RequiresApi(Build.VERSION_CODES.O)
     override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
-        recyclerView = binding.albumDetailRv
-        recyclerView.layoutManager = LinearLayoutManager(context)
+
 
         val txtDescription: TextView = view.findViewById(R.id.DescripcionAlbumDetails)
         txtDescription.text = args.myArg.description.toString()
 
         val txtName: TextView = view.findViewById(R.id.NameAlbumDetails)
         txtName.text = args.myArg.name.toString()
+
+        val txtPerformers: TextView = view.findViewById(R.id.PerformerNamesAlbumDetails)
+        txtPerformers.text = args.myArg.performerNames
+
+        val txtGenre: TextView = view.findViewById(R.id.GenreAlbumDetails)
+        txtGenre.text = args.myArg.genre.toString()
+
+        val formatter = DateTimeFormatter.ofPattern("dd-MM-yyyy")
+        val date = LocalDate.parse(args.myArg.releaseDate.toString().substring(0,10),  DateTimeFormatter.ISO_DATE)
+
+        val txtReleaseDate: TextView = view.findViewById(R.id.ReleaseDateAlbumDetails)
+        txtReleaseDate.text = date.format(formatter)
+
+        val txtRecordLabel: TextView = view.findViewById(R.id.RecordLabelAlbumDetails)
+        txtRecordLabel.text = args.myArg.recordLabel.toString()
+
+        val imgCover: ImageView = view.findViewById(R.id.imageViewAlbumDetails)
+        Picasso
+            .get()
+            .load(args.myArg.cover)
+            .into(imgCover)
     }
 
     override fun onDestroyView() {
