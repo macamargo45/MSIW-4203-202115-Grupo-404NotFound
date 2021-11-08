d8ae1a62e16c7365712a8e575b33f6b670802a86
Miguel Camargo <ma.camargo45@uniandes.edu.co>
2021-11-07 22:43:27 -0500

    Release/1.0.0 (#64)
    * Listar los albums (#46)
    
    * Listar los albums
    
    * Fix PR
    
    Co-authored-by: Andres Benavides <andres.benavides@eventsite.co>
    
    * Estructura inicial Detalle Albumes
    
    * misc file
    
    * Album Details funcionalidad básica
    
    * Feature crear vista detallada del álbum #5 (#47)
    
    * Estructura inicial Detalle Albumes
    
    * misc file
    
    * Album Details funcionalidad básica
    
    * Feature crear vista de catalogo de album #1 (#48)
    
    * Ajustes de estilos
    
    * Se agrega vista de lista de albumes
    
    * Correcciones para service adapter
    
    Co-authored-by: Carlos Garcia <cgarciasantander@gmail.com>
    
    * Mejoras al bottom bar (#49)
    
    Co-authored-by: Carlos Garcia <cgarciasantander@gmail.com>
    
    * UI Detalle de album completa
    
    * Quitando strings hardcodeados
    
    * Cambiando icono de la aplicación
    
    * Feature crear vista detallada del álbum #5 (#50)
    
    * Estructura inicial Detalle Albumes
    
    * misc file
    
    * Album Details funcionalidad básica
    
    * UI Detalle de album completa
    
    * Quitando strings hardcodeados
    
    * Cambiando icono de la aplicación
    
    * Version Inicial del file readme
    
    * Agregando folder APKs
    
    * Cambios adicionales al readme
    
    * Corrigiendo error de ortografía en el readme
    
    * Agregando link al backlog del producto
    
    * Update README.md
    
    * Arreglo de fechas para evitar fallo en Lollipop
    
    * Arreglo de fechas para evitar fallo en Lollipop
    
    * Feature crear vista detallada del álbum #5 (#61)
    
    * Estructura inicial Detalle Albumes
    
    * misc file
    
    * Album Details funcionalidad básica
    
    * UI Detalle de album completa
    
    * Quitando strings hardcodeados
    
    * Cambiando icono de la aplicación
    
    * Version Inicial del file readme
    
    * Agregando folder APKs
    
    * Cambios adicionales al readme
    
    * Corrigiendo error de ortografía en el readme
    
    * Agregando link al backlog del producto
    
    * Update README.md
    
    * Arreglo de fechas para evitar fallo en Lollipop
    
    * Arreglo de fechas para evitar fallo en Lollipop
    
    * Manejo de errores
    
    * Feature/implement repository pattern (#62)
    
    * Adding the album repository
    
    * Calling the repository in the view model
    
    * Feature/creacion pruebas e2e #3 (#63)
    
    * Adding .idea folder to the fit ignore
    
    * Adding dependencies for Espresso
    
    * Making the test for the album fragment
    
    * Adding record and play test
    
    * WIP
    
    * Adding the idli resoruce to the project
    
    * Adding dependencies
    
    * Adding the registerIdlingResource to the tests
    
    * Agregando pruebas adicionales
    
    * Arreglos de pruebas
    
    * Renamed testing classes
    
    * WIP
    
    * Change navigation top back, to goback function
    
    Co-authored-by: jmonterovalverdeMISO <j.monterov@uniandes.edu.co>
    
    Co-authored-by: Andres Benavides <f.benavidesv@uniandes.edu.co>
    Co-authored-by: Andres Benavides <andres.benavides@eventsite.co>
    Co-authored-by: jmonterovalverdeMISO <j.monterov@uniandes.edu.co>
    Co-authored-by: Carlos Garcia <cj.garcias1@uniandes.edu.co>
    Co-authored-by: Carlos Garcia <cgarciasantander@gmail.com>

---
+++ app/src/main/java/com/example/vinilos/views/AlbumDetailsFragment.kt
@@ -0,0 +1,120 @@
+package com.example.vinilos.views
+
+import android.os.Build
+import android.os.Bundle
+import android.util.Log
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import android.widget.ImageView
+import android.widget.TextView
+import android.widget.Toast
+import androidx.annotation.RequiresApi
+import androidx.fragment.app.Fragment
+import androidx.lifecycle.ViewModelProvider
+import androidx.navigation.findNavController
+import androidx.navigation.fragment.navArgs
+import com.example.vinilos.R
+import com.example.vinilos.databinding.AlbumDetailsFragmentBinding
+import com.example.vinilos.viewmodels.AlbumDetailsViewModel
+import com.squareup.picasso.Picasso
+import java.text.SimpleDateFormat
+
+class AlbumDetailsFragment : Fragment() {
+    private var _binding: AlbumDetailsFragmentBinding? = null
+    private val binding get() = _binding!!
+    private lateinit var viewModel: AlbumDetailsViewModel
+    private val args: AlbumDetailsFragmentArgs by navArgs()
+
+
+    override fun onCreateView(
+        inflater: LayoutInflater, container: ViewGroup?,
+        savedInstanceState: Bundle?
+    ): View {
+
+            _binding = AlbumDetailsFragmentBinding.inflate(inflater, container, false)
+            val view = binding.root
+            return view
+    }
+
+    override fun onActivityCreated(savedInstanceState: Bundle?) {
+        try {
+            super.onActivityCreated(savedInstanceState)
+            val activity = requireNotNull(this.activity) {
+                "You can only access the viewModel after onActivityCreated()"
+            }
+            viewModel =
+                ViewModelProvider(this, AlbumDetailsViewModel.Factory(activity.application)).get(
+                    AlbumDetailsViewModel::class.java
+                )
+            viewModel.album.observe(viewLifecycleOwner, {
+                it.apply {
+                    //Log.d("act", "ACA CREO QUE DEBERIA CARGAR LOS ALBUMES")
+                    //viewModelAdapter!!.albums = args
+                }
+            })
+            viewModel.eventNetworkError.observe(viewLifecycleOwner, { isNetworkError ->
+                if (isNetworkError) onNetworkError()
+            })
+        } catch (e: Exception) {
+            Log.println(Log.ERROR, "Error", e.message.toString())
+            val action = AlbumFragmentDirections.actionAlbumFragment2ToErrorMessageFragment()
+            view?.findNavController()?.navigate(action)
+        }
+    }
+
+    @RequiresApi(Build.VERSION_CODES.O)
+    override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
+
+        try {
+            val txtDescription: TextView = view.findViewById(R.id.DescripcionAlbumDetails)
+            txtDescription.text = args.myArg.description.toString()
+
+            val txtName: TextView = view.findViewById(R.id.NameAlbumDetails)
+            txtName.text = args.myArg.name.toString()
+
+            val txtPerformers: TextView = view.findViewById(R.id.PerformerNamesAlbumDetails)
+            txtPerformers.text = args.myArg.performerNames
+
+            val txtGenre: TextView = view.findViewById(R.id.GenreAlbumDetails)
+            txtGenre.text = args.myArg.genre.toString()
+
+            val parser = SimpleDateFormat("yyyy-MM-dd'T'HH:mm:ss")
+            val formatter = SimpleDateFormat("dd-MM-yyyy")
+            val date =
+                formatter.format(parser.parse(args.myArg.releaseDate.toString().substring(0, 19)))
+
+            val txtReleaseDate: TextView = view.findViewById(R.id.ReleaseDateAlbumDetails)
+            txtReleaseDate.text = date.format(formatter)
+
+            val txtRecordLabel: TextView = view.findViewById(R.id.RecordLabelAlbumDetails)
+            txtRecordLabel.text = args.myArg.recordLabel.toString()
+
+            val imgCover: ImageView = view.findViewById(R.id.imageViewAlbumDetails)
+            Picasso
+                .get()
+                .load(args.myArg.cover)
+                .into(imgCover)
+        } catch (e: Exception) {
+            Log.println(Log.ERROR, "Error", e.message.toString())
+            val action = AlbumFragmentDirections.actionAlbumFragment2ToErrorMessageFragment()
+            view.findNavController().navigate(action)
+        }
+    }
+
+    override fun onDestroyView() {
+        super.onDestroyView()
+        _binding = null
+    }
+
+    private fun onNetworkError() {
+        if (!viewModel.isNetworkErrorShown.value!!) {
+            val action = AlbumFragmentDirections.actionAlbumFragment2ToErrorMessageFragment()
+            view?.findNavController()?.navigate(action)
+            Toast.makeText(activity, "Network Error", Toast.LENGTH_LONG).show()
+            viewModel.onNetworkErrorShown()
+        }
+    }
+
+
+}
