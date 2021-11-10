84df21ea8905d3ccce119e6d1d7357501def910e
Andres Benavides <f.benavidesv@uniandes.edu.co>
2021-11-10 11:42:57 -0500

    Feature/creacion lista artistas hu03 (#66)
    * fucionar archivo idea
    
    * Listar los artistas
    
    Co-authored-by: Andres Benavides <andres.benavides@eventsite.co>

---
+++ app/src/main/java/com/example/vinilos/models/Musician.kt
@@ -0,0 +1,16 @@
+package com.example.vinilos.models
+
+import android.os.Parcelable
+import kotlinx.parcelize.Parcelize
+
+@Parcelize
+data class Musician (
+    val id:Int,
+    val name: String?,
+    val image: String?,
+    val description: String?,
+    val birthDate: String?,
+    val albums: MutableList<Album?>
+): Parcelable {
+    val albumsNames get() = this.albums.map { it?.name }.joinToString()
+}
