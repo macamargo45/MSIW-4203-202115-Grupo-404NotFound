3b6880985e7b4f60ed04cb03c644a97dd1a52ac1
Carlos Garcia <cj.garcias1@uniandes.edu.co>
2021-11-13 16:24:55 -0500

    Agregar pruebas de e2e para lista de musicos (#72)
    Co-authored-by: Carlos Garcia <cgarciasantander@gmail.com>

---
+++ app/src/androidTest/java/com/example/vinilos/MusicianListTest.kt
@@ -0,0 +1,96 @@
+package com.example.vinilos
+
+
+import android.view.View
+import android.view.ViewGroup
+import androidx.test.espresso.Espresso.onView
+import androidx.test.espresso.action.ViewActions.click
+import androidx.test.espresso.assertion.ViewAssertions.matches
+import androidx.test.espresso.matcher.ViewMatchers.*
+import androidx.test.filters.LargeTest
+import androidx.test.rule.ActivityTestRule
+import androidx.test.runner.AndroidJUnit4
+import org.hamcrest.Description
+import org.hamcrest.Matcher
+import org.hamcrest.Matchers.allOf
+import org.hamcrest.TypeSafeMatcher
+import org.hamcrest.core.IsInstanceOf
+import org.junit.Rule
+import org.junit.Test
+import org.junit.runner.RunWith
+
+@LargeTest
+@RunWith(AndroidJUnit4::class)
+class MusicianListTest {
+
+    @Rule
+    @JvmField
+    var mActivityTestRule = ActivityTestRule(MainActivity::class.java)
+
+    @Test
+    fun musicianListTest() {
+        val bottomNavigationItemView = onView(
+            allOf(
+                withId(R.id.navigation_musicos), withContentDescription("Músicos"),
+                childAtPosition(
+                    childAtPosition(
+                        withId(R.id.navigationView),
+                        0
+                    ),
+                    1
+                ),
+                isDisplayed()
+            )
+        )
+        bottomNavigationItemView.perform(click())
+
+        // Debe contener una lista de musicos con un elemento
+        val musicianRv = onView(
+            allOf(
+                withId(R.id.musicianRv),
+                isDisplayed()
+            )
+        )
+        musicianRv.check(matches(hasChildCount(1)))
+
+        // Debe contener un musico de nombre Rubén Blades Bellido de Luna
+        val textView = onView(
+            allOf(
+                withId(R.id.musicianName),
+                withText("Rubén Blades Bellido de Luna"),
+                withContentDescription("100"),
+                withParent(withParent(IsInstanceOf.instanceOf(android.view.ViewGroup::class.java))),
+                isDisplayed()
+            )
+        )
+        textView.check(matches(withText("Rubén Blades Bellido de Luna")))
+
+        // Debe contener la imagen del musico Rubén Blades Bellido de Luna
+        val imageView = onView(
+            allOf(
+                withId(R.id.imageView), withContentDescription("Imagen del musico"),
+                withParent(withParent(IsInstanceOf.instanceOf(androidx.cardview.widget.CardView::class.java))),
+                isDisplayed()
+            )
+        )
+        imageView.check(matches(isDisplayed()))
+    }
+
+    private fun childAtPosition(
+        parentMatcher: Matcher<View>, position: Int
+    ): Matcher<View> {
+
+        return object : TypeSafeMatcher<View>() {
+            override fun describeTo(description: Description) {
+                description.appendText("Child at position $position in parent ")
+                parentMatcher.describeTo(description)
+            }
+
+            public override fun matchesSafely(view: View): Boolean {
+                val parent = view.parent
+                return parent is ViewGroup && parentMatcher.matches(parent)
+                        && view == parent.getChildAt(position)
+            }
+        }
+    }
+}
