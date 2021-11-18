b9d02bcbfbbe6a9c9b3006455bee2117390d221b
Andres Benavides <f.benavidesv@uniandes.edu.co>
2021-11-18 08:40:19 -0500

    Feature/corrutinas (#85)
    * Cambio a corrutinas para consultar los endpoints

--- app/src/main/java/com/example/vinilos/repositories/CollectorsRepository.kt
+++ app/src/main/java/com/example/vinilos/repositories/CollectorsRepository.kt
@@ -1,20 +1,12 @@
 package com.example.vinilos.repositories
 
 import android.app.Application
-import com.android.volley.VolleyError
-import com.example.vinilos.models.Album
 import com.example.vinilos.models.Collector
 import com.example.vinilos.network.NetworkServiceAdapter
 
 class CollectorsRepository(val application: Application) {
-    fun refreshData(callback: (List<Collector>) -> Unit, onError: (VolleyError) -> Unit) {
+    suspend fun refreshData(): List<Collector>{
         //Determinar la fuente de datos que se va a utilizar. Si es necesario consultar la red, ejecutar el siguiente código
-        NetworkServiceAdapter.getInstance(application).getCollectors(
-            {
-                //Guardar los coleccionistas de la variable it en un almacén de datos local para uso futuro
-                callback(it)
-            },
-            onError
-        )
+        return NetworkServiceAdapter.getInstance(application).getCollectors()
     }
 }
