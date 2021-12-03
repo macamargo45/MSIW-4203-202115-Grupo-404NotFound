cc54b032bb213891b441fb73381531918fd50fec
Miguel Camargo <ma.camargo45@uniandes.edu.co>
2021-12-03 17:42:37 -0500

    feat: finish the add album to musician HU15

--- app/src/main/java/com/example/vinilos/viewmodels/AddAlbumToMusicianViewModel.kt
+++ app/src/main/java/com/example/vinilos/viewmodels/AddAlbumToMusicianViewModel.kt
@@ -3,19 +3,26 @@ package com.example.vinilos.viewmodels
 import android.app.Application
 import androidx.lifecycle.*
 import com.example.vinilos.models.Album
-import com.example.vinilos.repositories.AddAlbumToMusicianRepository
+import com.example.vinilos.repositories.AlbumRepository
+import com.example.vinilos.repositories.MusiciansRepository
 import kotlinx.coroutines.Dispatchers
 import kotlinx.coroutines.launch
 import kotlinx.coroutines.withContext
 
 class AddAlbumToMusicianViewModel(application: Application) : AndroidViewModel(application) {
-    private val addAlbumToMusicianRepository = AddAlbumToMusicianRepository(application)
+    private val albumRepository = AlbumRepository(application)
+    private val musicianRepository = MusiciansRepository(application)
 
     private val _albums = MutableLiveData<List<Album>>()
     private var _albumId = MutableLiveData<Int>()
 
     val albums: LiveData<List<Album>>
         get() = _albums
+
+    val albumId: MutableLiveData<Int>
+        get() {
+            return _albumId
+        }
 
     private var _eventNetworkError = MutableLiveData(false)
 
@@ -33,10 +40,10 @@ class AddAlbumToMusicianViewModel(application: Application) : AndroidViewModel(a
 
     private fun refreshDataFromNetwork() {
         try {
-            viewModelScope.launch (Dispatchers.Default) {
+            viewModelScope.launch(Dispatchers.Default) {
                 try {
                     withContext(Dispatchers.IO) {
-                        val data = addAlbumToMusicianRepository.refreshData()
+                        val data = albumRepository.refreshData()
                         _albums.postValue(data)
                     }
                     _eventNetworkError.postValue(false)
@@ -46,8 +53,7 @@ class AddAlbumToMusicianViewModel(application: Application) : AndroidViewModel(a
                     _isNetworkErrorShown.postValue(true)
                 }
             }
-        }
-        catch (e:Exception){
+        } catch (e: Exception) {
             _eventNetworkError.value = true
         }
     }
@@ -58,10 +64,10 @@ class AddAlbumToMusicianViewModel(application: Application) : AndroidViewModel(a
 
     fun addAlbumToMusician(idAlbum: Int, idMusician: Int) {
         try {
-            viewModelScope.launch (Dispatchers.Default) {
+            viewModelScope.launch(Dispatchers.Default) {
                 try {
                     withContext(Dispatchers.IO) {
-                        val data = addAlbumToMusicianRepository.addAlbumToMusician(idAlbum, idMusician)
+                        val data = musicianRepository.addAlbumToMusician(idAlbum, idMusician)
                         _albumId.postValue(data)
                     }
                     _eventNetworkError.postValue(false)
@@ -71,8 +77,7 @@ class AddAlbumToMusicianViewModel(application: Application) : AndroidViewModel(a
                     _isNetworkErrorShown.postValue(true)
                 }
             }
-        }
-        catch (e:Exception){
+        } catch (e: Exception) {
             _eventNetworkError.value = true
         }
     }
